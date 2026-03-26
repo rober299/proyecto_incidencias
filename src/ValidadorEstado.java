72a5738de5130cb0fa192218e223fc0a3205825b
rober299 <robertochislea9@gmail.com>
2026-03-26 17:22:44 +0100

    refactor: mejora legibilidad del validador extrayendo variables

--- src/ValidadorEstado.java
+++ src/ValidadorEstado.java
@@ -1,11 +1,9 @@
 public class ValidadorEstado {
     public static boolean esTransicionValida(String origen, String destino) {
-        if (origen.equals("ABIERTA") && destino.equals("EN_PROGRESO")) {
-            return true;
-        }
-        if (origen.equals("EN_PROGRESO") && destino.equals("CERRADA")) {
-            return true;
-        }
-        return false;
+
+        boolean deAbiertaAProgreso = origen.equals("ABIERTA") && destino.equals("EN_PROGRESO");
+        boolean deProgresoACerrada = origen.equals("EN_PROGRESO") && destino.equals("CERRADA");
+
+        return deAbiertaAProgreso || deProgresoACerrada;
     }
 }
