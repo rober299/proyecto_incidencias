878082a2f93254074d38328a82f9f4d84978e32c
rober299 <robertochislea9@gmail.com>
2026-03-26 13:31:33 +0100

    feat: implementa la logica minima para hacer pasar los tests

--- src/ValidadorEstado.java
+++ src/ValidadorEstado.java
@@ -1,5 +1,11 @@
 public class ValidadorEstado {
     public static boolean esTransicionValida(String origen, String destino) {
+        if (origen.equals("ABIERTA") && destino.equals("EN_PROGRESO")) {
+            return true;
+        }
+        if (origen.equals("EN_PROGRESO") && destino.equals("CERRADA")) {
+            return true;
+        }
         return false;
     }
 }
